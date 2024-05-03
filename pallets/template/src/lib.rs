ba0c46f36e83e5a143210014306fd2d721733375
Ganesh oli <ganesh@thaiofficefurniture.com>
2024-05-03 19:03:00 +0545

    update

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -13,121 +13,87 @@ mod tests;
 #[cfg(feature = "runtime-benchmarks")]
 mod benchmarking;
 pub mod weights;
-pub use weights::*;
 use frame_support::sp_runtime::offchain::{http, Duration};
-
+pub use weights::*;
 
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
-
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
 
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
-		/// Offchain worker enety point.
 		fn offchain_worker(_block_number: BlockNumberFor<T>) {
-			Self::fetch_properties().unwrap();
-			log::info!("Hello world fromoffchain workers!");
+			let _ = Self::fetch_n_parse().unwrap();
 		}
 	}
 
-	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
-		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-		/// Type representing the weight of this pallet
 		type WeightInfo: WeightInfo;
 		#[pallet::constant]
-        type StringLimit: Get<u32>;
+		type StringLimit: Get<u32>;
 	}
 
-	// The pallet's runtime storage items.
-	// https://docs.substrate.io/main-docs/build/runtime-storage/
 	#[pallet::storage]
 	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
 	pub type Something<T> = StorageValue<_, u32>;
 
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/main-docs/build/events-errors/
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
 		SomethingStored { something: u32, who: T::AccountId },
 	}
+
+	// #[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize, Encode, Decode, Default, RuntimeDebug, scale_info::TypeInfo))]
 
 	#[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
     #[derive(Encode, Decode, Clone, PartialEq, Eq, MaxEncodedLen, RuntimeDebug, TypeInfo)]
     #[scale_info(skip_type_params(T))]
-	pub struct Property {
-		id: i64,
-		bedrooms: i32,
-		bathrooms: i32,
-		number_of_images: i32,
-		number_of_floorplans: i32,
-		number_of_virtual_tours: i32,
-		summary:i32,
-		display_address:i32,
-		country_code:i32,
-		location:i32,
+	struct HackerNewsInfo<T: Config> {
+		by: BoundedVec<u8, <T as Config>::StringLimit>,
+		title: BoundedVec<u8, <T as Config>::StringLimit>,
+		url: BoundedVec<u8, <T as Config>::StringLimit>,
+		descendants: u32,
 	}
-	
 
-	// Errors inform users that something went wrong.
+
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Error names should be descriptive.
 		NoneValue,
-		/// Errors should have helpful documentation associated with them.
 		StorageOverflow,
+		HttpFetchingError,
+		DeserializeToStrError,
+		DeserializeToObjError,
 	}
 
-	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::call_index(0)]
 		#[pallet::weight(T::WeightInfo::do_something())]
 		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/main-docs/build/origins/
 			let who = ensure_signed(origin)?;
 
 			// Update storage.
 			<Something<T>>::put(something);
 
-			// Emit an event.
 			Self::deposit_event(Event::SomethingStored { something, who });
-			// Return a successful DispatchResultWithPostInfo
 			Ok(())
 		}
 
-		/// An example dispatchable that may throw a custom error.
 		#[pallet::call_index(1)]
 		#[pallet::weight(T::WeightInfo::cause_error())]
 		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
 			let _who = ensure_signed(origin)?;
 
-			// Read a value from storage.
 			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
 				None => return Err(Error::<T>::NoneValue.into()),
 				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
 					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
 					<Something<T>>::put(new);
 					Ok(())
 				},
@@ -135,44 +101,61 @@ pub mod pallet {
 		}
 	}
 
+	const HTTP_REMOTE_REQUEST: &str = "https://hacker-news.firebaseio.com/v0/item/9129911.json";
+
+	const FETCH_TIMEOUT_PERIOD: u64 = 3000; // in milli-seconds
+	use scale_info::prelude::{string::String, vec::Vec};
+
 	impl<T: Config> Pallet<T> {
-		fn fetch_properties() -> Result<(), http::Error> {
-			let deadline = sp_io::offchain::timestamp().add(Duration::from_millis(2_000));
-			let request = 
-				http::Request::get("https://ipfs.io/ipfs/QmRYy9yPQYHUH2He18MFnnDEyfwST27GwywLB8N2jJ9fo2?filename=p.json");
-			
-			let pending = request.deadline(deadline).send().map_err(|_| http::Error::IoError)?;
-			let response = pending.try_wait(deadline).map_err(|_| http::Error::DeadlineReached)??;
-
-			if response.code != 200 {
-				log::warn!("Unexpected status code: {}", response.code);
-				return Err(http::Error::Unknown)
-			}
-
-			let body = response.body().collect::<Vec<u8>>();
-
-			let body_str = sp_std::str::from_utf8(&body).map_err(|_| {
-				log::warn!("No UTF8 body");
-				http::Error::Unknown
+		fn fetch_n_parse() -> Result<HackerNewsInfo<T>, Error<T>> {
+			let resp_bytes = Self::fetch_from_remote().map_err(|e| {
+				log::error!("fetch_from_remote error: {:?}", e);
+				<Error<T>>::HttpFetchingError
 			})?;
-	
-			let price = match Self::parse_price(body_str) {
-				Some(price) => Ok(price),
-				None => {
-					log::warn!("Unable to extract price from the response: {:?}", body_str);
-					Err(http::Error::Unknown)
-				},
-			}?;
 
-			log::info!("{:?}", price);
 
-			Ok(())
+			let resp_strr =
+				String::from_utf8(resp_bytes).map_err(|_| <Error<T>>::DeserializeToStrError)?;
+
+			let resp_str = resp_strr.as_str();
+			log::info!("fetch_n_parse: {}", resp_str);
+
+			let info: HackerNewsInfo<T> =
+				serde_json::from_str(&resp_str).map_err(|_| <Error<T>>::DeserializeToObjError)?;
+			Ok(info)
 		}
 
-		fn parse_price(price_str: &str) -> Option<Property> {
+		fn fetch_from_remote() -> Result<Vec<u8>, Error<T>> {
+			let request = http::Request::get(HTTP_REMOTE_REQUEST);
 
-			let my_struct: Property = serde_json::from_str(&price_str).expect("Failed to deserialize JSON");
-			Some(my_struct)
+			let timeout =
+				sp_io::offchain::timestamp().add(Duration::from_millis(FETCH_TIMEOUT_PERIOD));
+
+			let pending = request
+				.deadline(timeout) // Setting the timeout time
+				.send() // Sending the request out by the host
+				.map_err(|e| {
+					log::error!("{:?}", e);
+					<Error<T>>::HttpFetchingError
+				})?;
+
+			let response = pending
+				.try_wait(timeout)
+				.map_err(|e| {
+					log::error!("{:?}", e);
+					<Error<T>>::HttpFetchingError
+				})?
+				.map_err(|e| {
+					log::error!("{:?}", e);
+					<Error<T>>::HttpFetchingError
+				})?;
+
+			if response.code != 200 {
+				log::error!("Unexpected http request status code: {}", response.code);
+				return Err(<Error<T>>::HttpFetchingError);
+			}
+
+			Ok(response.body().collect::<Vec<u8>>())
 		}
 	}
 }
